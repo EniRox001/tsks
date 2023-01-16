70a5e8c1522f8592acb0686873aca65366f41e55
enirox <enirox001@gmail.com>
2023-01-16 22:59:48 +0100

    changed drawer design

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Dialog, Typography, Box, Divider, List, ListItem, ListItemButton, ListItemText, Stack, Link } from "@mui/material";
+import { IconButton, Dialog, Typography, Box, Divider, List, ListItem, ListItemButton, ListItemText, Stack, Link, makeStyles } from "@mui/material";
 import { Menu, Close} from '@mui/icons-material';
 import { useState } from "react";
 import NavLoginButton from "./Buttons/NavLoginButton";
@@ -31,15 +31,11 @@ export default function DrawerButton() {
                 fullScreen
                 open={open}
                 onClose={handleClose}
-                style={{ backgroundColor: 'transparent' }}
-                PaperProps={{
-                    style: {
-                      backgroundColor: "black",
-                      boxShadow: "none"
-                    },
-                  }}
                 >
-                    <Box>
+                    <Box
+                        bgcolor='#121212'
+                        width='100vw'
+                        height='100vh'>
                         <Stack direction="row" justifyContent='flex-end'>
                             <IconButton aria-label="close" onClick={handleClose} sx={{mx : 2, my : 1}}>
                                 <Close />
