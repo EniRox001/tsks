349bd3c20ad2810e655794c57158802c470d34f9
enirox <enirox001@gmail.com>
2023-01-17 14:19:12 +0100

    added login dialog onChange functionality

--- src/components/Functions/LoginDialog.tsx
+++ src/components/Functions/LoginDialog.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogTitle, Button, Typography, DialogContent, TextField, Stack, Link, IconButton } from '@mui/material/';
 import { Close } from '@mui/icons-material';
+import { ChangeEvent, useState } from 'react';
 
 export interface LoginDialogProps {
   open: boolean;
@@ -7,39 +8,56 @@ export interface LoginDialogProps {
 }
 
 export default function LoginDialog(props: LoginDialogProps) {
-  const { onClose, open } = props;
+    const [userData, setUserData] = useState({
+        username: '',
+        password: ''
+    })
 
-  const handleClose = () => {
-    onClose();
-  };
+    const handleChange = ( e : ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setUserData({
+            ...userData,
+            [e.target.name] : value
+        });
+    };
 
-  return (
-    <Dialog onClose={handleClose} open={open} fullWidth={true}>
-        <Stack direction="row" justifyContent='flex-end'>
-            <IconButton aria-label="close" onClick={handleClose} sx={{mx : 2, my : 1}}>
-                <Close />
-            </IconButton>
-        </Stack>
-        <DialogTitle>
-            <Typography variant="h4">Enter Details to Login</Typography>
-        </DialogTitle>
-        <DialogContent>
-            <Stack spacing={2}>
-                <TextField required id="username" label="Username" variant="outlined" />
-                <TextField required id="password" label="Password" type="password" autoComplete="current-password" variant="outlined" />
-                <Button variant='outlined' sx={{
-                    p: 2
-                }}>Login</Button>
-                <Stack direction='row' spacing={1}>
-                    <Typography>Don't have an account?</Typography>
-                    <Link 
-                        href="#"
-                        underline='none'
-                        >Sign up
-                    </Link>
-                </Stack>
+    const handleSubmit = () => {
+        //Create server function to add login functionality
+    }
+
+    const { onClose, open } = props;
+
+    const handleClose = () => {
+        onClose();
+    };
+
+    return (
+        <Dialog onClose={handleClose} open={open} fullWidth={true}>
+            <Stack direction="row" justifyContent='flex-end'>
+                <IconButton aria-label="close" onClick={handleClose} sx={{mx : 2, my : 1}}>
+                    <Close />
+                </IconButton>
             </Stack>
-        </DialogContent>
-    </Dialog>
-  );
+            <DialogTitle>
+                <Typography variant="h4">Enter Details to Login</Typography>
+            </DialogTitle>
+            <DialogContent>
+                <Stack spacing={2}>
+                    <TextField required id="username" name="username" label="Username" variant="outlined" value={userData.username} onChange={handleChange}/>
+                    <TextField required id="password" name="password" label="Password" type="password" autoComplete="current-password" variant="outlined" value={userData.password} onChange={handleChange}/>
+                    <Button variant='outlined' sx={{
+                        p: 2
+                    }}>Login</Button>
+                    <Stack direction='row' spacing={1}>
+                        <Typography>Don't have an account?</Typography>
+                        <Link 
+                            href="#"
+                            underline='none'
+                            >Sign up
+                        </Link>
+                    </Stack>
+                </Stack>
+            </DialogContent>
+        </Dialog>
+    );
 }
